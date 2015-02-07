56fb8d70f01876c25079edf1e79b15f3e69713d0
Bruno <brunorios1@gmail.com>
2015-02-07 15:43:44 -0200

    gulp task for watch and compile scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,22 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 
-gulp.task('sass', function () {
-  gulp.src('src/scss/*.scss')
+var paths = {
+  sass: ['src/scss/**/*.scss']
+  // images: 'client/img/**/*'
+};
+
+// Styles
+gulp.task('styles', function () {
+  gulp.src(paths.sass)
     .pipe(sass())
     .pipe(gulp.dest('.tmp/css'));
 });
+
+// Rerun the task when a file changes
+gulp.task('watch', function() {
+  gulp.watch(paths.sass, ['styles']);
+});
+
+// The default task (called when you run `gulp` from cli)
+gulp.task('default', ['watch']);
