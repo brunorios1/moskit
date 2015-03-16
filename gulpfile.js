d25971d4933b35ae5a9c1b7b4c8287fb0d1fb6b2
Bruno <brunorios1@gmail.com>
2015-03-16 01:39:54 -0300

    implements BrowserSync

--- gulpfile.js
+++ gulpfile.js
@@ -7,25 +7,32 @@ var please = require('gulp-pleeease');
 var eslint = require('gulp-eslint');
 var uglify = require('gulp-uglify');
 var size = require('gulp-size');
-var runSequence = require('run-sequence');
+// var runSequence = require('run-sequence');
 var useref = require('gulp-useref');
 var gulpif = require('gulp-if');
 var minifyHTML = require('gulp-minify-html');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
 
 var paths = {
   styles: ['src/core/**/*.scss', 'src/custom/**/*.scss'],
   scripts: ['src/core/**/*.js', 'src/custom/**/*.js'],
-  html: 'src/custom/**/*.html',
+  html: '*.html',
+  tmp: 'tmp',
   dist: 'dist',
 };
 
-gulp.task('clean', function(cb) {
+gulp.task('serve-clean', function(cb) {
+  del([paths.tmp + '/*'], cb);
+});
+
+gulp.task('build-clean', function(cb) {
   del([paths.dist + '/*'], cb);
 });
 
 // Styles
-gulp.task('styles', function() {
-  gulp.src(paths.styles)
+gulp.task('serve-styles', ['serve-clean'], function() {
+  var stream = gulp.src(paths.styles)
     // lint scss code
     .pipe(scsslint({
       'bundleExec': true
@@ -38,51 +45,78 @@ gulp.task('styles', function() {
     }))
     .pipe(please({
       "autoprefixer": { "browsers": ["last 2 versions"] },
-      "mqpacker": true
+      "mqpacker": true,
+      "minifier": false
     }))
-    .pipe(sourcemaps.write('../../maps'))
+    .pipe(sourcemaps.write())
     // Compiles to build folder
-    .pipe(gulp.dest(paths.dist + '/css'))
+    .pipe(gulp.dest(paths.tmp + '/css'))
+    // auto-inject into browsers
+    .pipe(gulpif('*.css', reload({stream: true})));
+  return stream;
+});
+
+// Styles
+gulp.task('build-styles', ['build-clean'], function() {
+  var stream = gulp.src(paths.styles)
+    .pipe(sass({
+      // "keep gulp from stopping every time you mess up your sass"
+      errLogToConsole: true
+    }))
+    // Compiles to build folder
+    .pipe(gulp.dest(paths.tmp + '/css'))
     // Size
     .pipe(size({showFiles: true, title: 'styles'}));
+  return stream;
 });
 
 // Scripts
-gulp.task('scripts', function() {
-    // Note: To have the process exit with an error code (1) on
-    //  lint error, return the stream and pipe to failOnError last.
-    gulp.src(paths.scripts)
-      .pipe(eslint())
-      .pipe(eslint.format())
-      // .pipe(eslint.failOnError())
+gulp.task('serve-scripts', function() {
+  // Note: To have the process exit with an error code (1) on
+  //  lint error, return the stream and pipe to failOnError last.
+  gulp.src(paths.scripts)
+    .pipe(eslint())
+    .pipe(eslint.format())
+    // .pipe(eslint.failOnError())
 });
 
-// Html
-gulp.task('html', function () {
+// The default task (called when you run `gulp` from cli)
+gulp.task('build-html', ['build-styles'], function () {
   var assets = useref.assets();
 
-  return gulp.src(paths.html)
+  // return gulp.src(paths.html)
+  var stream = gulp.src(paths.html)
     .pipe(assets)
       // Concatenates and minifies JS
     .pipe(gulpif('*.js', uglify()))
+    .pipe(gulpif('*.css', please({
+      "autoprefixer": { "browsers": ["last 2 versions"] },
+      "mqpacker": true
+    })))
     .pipe(assets.restore())
     .pipe(useref())
-    // Minifies HTML
     .pipe(minifyHTML())
     // Compiles HTML to build folder
     .pipe(gulp.dest(paths.dist))
     // Size
     .pipe(size({showFiles: true, title: 'html'}));
+    return stream;
 });
 
-// Check for changes and rerun tasks when a file changes
-gulp.task('watch', ['default'], function() {
-  gulp.watch(paths.styles, ['styles']);
-  gulp.watch(paths.scripts, ['scripts']);
-  gulp.watch(paths.html, ['html']);
+// Browser Sync
+gulp.task('browser-sync', ['serve-styles'], function() {
+  browserSync({
+    server: ["./", "./src", "./tmp"]
+  });
 });
 
-// The default task (called when you run `gulp` from cli)
-gulp.task('default', function(cb) {
-  runSequence('clean', ['styles', 'scripts', 'html'], cb);
+// Watch
+gulp.task('watch', ['browser-sync'], function() {
+  gulp.watch(paths.styles, ['serve-styles']);
+  gulp.watch(paths.scripts, ['serve-scripts', reload]);
+  gulp.watch(paths.html, reload);
 });
+
+// Build
+gulp.task('build', ['build-html']);
+gulp.task('default', ['build']);
