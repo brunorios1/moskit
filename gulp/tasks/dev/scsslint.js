88f171c85623e654b954295c0efdcd8aecc79b01
Bruno <brunorios1@gmail.com>
2015-03-25 00:31:33 -0300

    responsive heights for the sticky footer

--- gulp/tasks/dev/scsslint.js
+++ gulp/tasks/dev/scsslint.js
@@ -2,7 +2,6 @@ var gulp = require('gulp');
 var cache = require('gulp-cached');
 var scsslint = require('gulp-scss-lint');
 var config = require('../../config').scsslint;
-//var remember = require('gulp-remember');
 
 // Sass
 gulp.task('scsslint', function() {
